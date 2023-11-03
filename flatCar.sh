fd8a2048eb921e1c376bf5eb9f9a077edc741530
eyeVish <vishuash@gmail.com>
2023-11-03 12:01:04 +0530

    Update flatCar.sh

--- flatCar.sh
+++ flatCar.sh
@@ -1,9 +1,12 @@
+# ======================================================================================
 # This file downloads the default ignition file to install k3s
 # after the run on this script from you windows installation 
 # where you choose to run the deplu scripts - run the following
 # mkdir %userprofile%\.kube
 # ssh core@xpinode1 "sudo cat /etc/rancher/k3s/k3s.yaml" > %userprofile%\.kube\config
-rm -f ignition.json || curl https://github.com/eyevish/eyevish/raw/main/ignition.json -o ignition.json
+# ======================================================================================
+
+rm -f ignition.json || curl -sfL https://github.com/eyevish/eyevish/raw/main/ignition.json -o ignition.json
 sudo flatcar-install -d /dev/sda -i ignition.json
 echo -e "\n=============================================================\n"
 echo -e "\n - To access the Kubernetes server and deploy your IMM run the following 
